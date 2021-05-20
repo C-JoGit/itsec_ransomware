ba7d12dca3f420151bfab990f52525f1be809810
Louisaebert <77397998+Louisaebert@users.noreply.github.com>
2021-05-20 10:45:10 +0200

    added the messagebox file

---
+++ message_box.py
@@ -0,0 +1,8 @@
+import tkinter
+from tkinter import messagebox
+# hide main window
+
+root = tkinter.Tk()
+# message box display
+messagebox.showwarning("Warning","Your important files have been encrypted! Pay 75$ to get the key")
+
